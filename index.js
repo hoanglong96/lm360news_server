5a9687c5cb9a79d34ff058e5dc6c02894ce80812
Hoàng Long <long11hq@gmail.com>
2018-03-08 09:53:58 +0700

    update 8/3

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var Diacritics = require('diacritic');
-var request = require('request');
-var cheerio = require('cheerio');
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-//Model
-var Home = require('./model/home')
-var VCS = require('./model/vcs')
+var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
+app.use(bodyParser.json());
 
-//Connet moogodb
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://hlong96:Hoanglong96@ds251588.mlab.com:51588/lm360news'
-  , { useMongoClient: true });
-//
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
 
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://hlong96:Hoanglong96@ds251588.mlab.com:51588/lm360news", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-app.set('port', (process.env.PORT || 8000));
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
 });
-
-
