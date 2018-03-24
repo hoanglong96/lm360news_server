73e47954b59ed03df859e10e56ac552aabcaa10e
Hoàng Long <long11hq@gmail.com>
2018-03-24 11:53:50 +0700

    update

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var mongoose = require('mongoose');
-var Diacritics = require('diacritic');
-var request = require('request');
-var cheerio = require('cheerio');
 
 
 var app = express();
 app.use(bodyParser.json());
-
-//Connet moogodb
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://hlong96:Hoanglong96@ds251588.mlab.com:51588/lm360news'
-  , { useMongoClient: true });
-//
 
   // Initialize the app.
   var server = app.listen(process.env.PORT || 8080, function () {
